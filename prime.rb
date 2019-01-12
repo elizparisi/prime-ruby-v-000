5bee248b0a67219184106271754c1f523cfc53e4
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-01-12 22:42:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 # Add  code here!
+
+
 def prime?(num)
-  return false if num <= 1
- Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
- true
+  return false if num == 0 || num == 1
+  for i in 2..Math.sqrt(num)
+    return false if num % i == 0
+  end
+  true
 end
